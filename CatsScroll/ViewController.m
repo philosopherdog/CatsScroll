d9df6dd685a45d46d6434be326eaf440f0672619
steve thompson <steven.thompson.ca@gmail.com>
2018-01-29 16:19:22 -0500

    Initial

--- CatsScroll/ViewController.m
+++ CatsScroll/ViewController.m
@@ -1,28 +1,55 @@
-//
-//  ViewController.m
-//  CatsScroll
-//
-//  Created by steve on 2018-01-29.
-//  Copyright © 2018 steve. All rights reserved.
-//
+  //
+  //  ViewController.m
+  //  CatsScroll
+  //
+  //  Created by steve on 2018-01-29.
+  //  Copyright © 2018 steve. All rights reserved.
+  //
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UIScrollViewDelegate>
+@property (nonatomic) UIScrollView *sv;
 @end
 
 @implementation ViewController
 
+#define ivtag 100
+
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  self.sv = [[UIScrollView alloc] init];
+  [self.view addSubview:self.sv];
+  UIImageView *iv = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cats"]];
+  iv.tag = ivtag;
+  [self.sv addSubview:iv];
+  self.sv.delegate = self;
+  self.sv.minimumZoomScale = 0.25;
+  self.sv.maximumZoomScale = 3.0;
 }
 
+- (void)viewDidLayoutSubviews {
+  self.sv.frame = self.view.frame;
+  UIImageView *iv = [self.view viewWithTag:ivtag];
+  self.sv.contentSize = iv.bounds.size;
+}
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+  
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+  
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+  
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+  return [self.view viewWithTag:ivtag];
 }
 
 
